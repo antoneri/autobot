83e030aaacde1ab579a308ad5a2cece3279ce267
Anton Eriksson <tfy12aen@cs.umu.se>
2014-11-26 21:23:25 +0100

    Major refactoring

--- autobot.pl
+++ autobot.pl
@@ -7,6 +7,7 @@
 #
 use strict;
 use warnings;
+
 use Irssi;
 use LWP::UserAgent;
 use XML::Simple 'XMLin';
@@ -14,105 +15,106 @@ use Text::Levenshtein 'distance';
 
 use vars qw($VERSION %IRSSI);
 
-$VERSION = "0.1";
-%IRSSI = (
-  authors     => "Anton Eriksson",
-  contact     => "anton\@rizon",
-  name        => "autobot",
-  date        => "2014-01-23",
-  updated     => "2014-11-29",
-  description => "Auto reply IRC-bot/Race of shapeshifting robots.",
-  license     => "BSD 2-clause",
-  url         => "http://www.github.com/antoneri/autobot/"
-);
+$VERSION = "0.2";
+%IRSSI = (authors     => "Anton Eriksson",
+          contact     => "anton\@rizon",
+          name        => "autobot",
+          date        => "2014-01-23",
+          updated     => "2014-11-26",
+          description => "Auto reply IRC-bot/Race of shapeshifting robots.",
+          license     => "BSD 2-clause",
+          url         => "http://www.github.com/antoneri/autobot/");
 
-sub autobot {
-  my ($server, $msg, $nick, $address, $target) = @_;
+sub auto_op {
+  my ($srv, $msg, $nick, $addr, $target) = @_;
 
-  my $response = undef;
+  my @opers = qw(Ades anton Angan hunky\\ Tomas);
+  my %hashop = map { $_ => 1 } @opers;
+  
+  if ($msg eq "op plz") {
+    if (exists($hashop{$nick})) {
+      $srv->command("OP $target $nick");
+    } else {
+      $srv->command("MSG $target Nope.");
+    }
+  }
+}
+
+sub dice {
+  my ($srv, $msg, $nick, $addr, $target) = @_;
+
+  if ($msg =~ /^!dice ([^;]+(?:;[^;]+)+)$/i) {
+    
+    my @choices = split(';', $1);
+    my $i = int(rand(scalar @choices));
+
+    $choices[$i] =~ s/^\s+|\s+$//g;  # trim whitespace
+
+    $srv->command("MSG $target Tärningen bestämmer: $choices[$i]")
+  }
+}
+
+### We don't want 'spotify' and 'get_page_title' to
+### both react to spotify http uri:s
+sub uri_handler{
+  my ($srv, $msg, $nick, $addr, $target) = @_;
 
   if ($msg =~ /(?!https?:\/\/open.spotify.com\/|spotify:)
                (album|artist|track)
                [:\/]
                ([a-zA-Z0-9]+)\/?/ix) {
-    $response = spotify($1, $2);
-  } elsif ($msg =~ /(
-                      (?:https?:\/\/)?
-                      (?:[\w\d-]+\.)*
-                      ([\w\d-]+)
-                      \.[a-z]{2,6}.*
-                    )\b/ix) {
-    my $title = get_page_title($1);
+                 
+    my $spotify = spotify($1, $2);
+    $srv->command("MSG $target $spotify") if $spotify;
+    
+  } elsif ($msg =~ /((?:https?:\/\/)?(?:[\w\d-]+\.)*
+                    ([\w\d-]+)\.[a-z]{2,6}.*)\b/ix) {
 
-    if ($title) {
-      my $domain = $2;
-      my @words = split(' ', $title);
-      my $pos = 0;
-      my $titlepos = undef;
+    my $title = get_page_title($1, $2);
+    $srv->command("MSG $target $title") if $title;
 
-      foreach (@words) {
-        if (distance(lc($_), lc($domain)) < 2) {
-          $titlepos = $pos;
-        }
-        $pos += 1;
-      }
-
-      if ($titlepos) {
-        $words[$titlepos] =~ s/^[,\.]|[,\.]$//;
-
-        if ($words[$titlepos-1] && $words[$titlepos-1] =~ "[-\|]") {
-          $title = join(' ', @words[0..$titlepos-2]);
-        } elsif ($words[$titlepos+1] && $words[$titlepos+1] =~ "[-\|]") {
-          $title = join(' ', @words[$titlepos+2..-1]);
-        }
-
-        $title = "[".$words[$titlepos]."] ".$title;
-      }
-
-      $response = $title;
-    }
-  } elsif ($msg =~ /^!dice ([^;]+(?:;[^;]+)+)$/i) {
-    my @choices = split(';', $1);
-    my $num = scalar @choices;
-    my $choice = int(rand($num));
-    $choices[$choice] =~ s/^\s+|\s+$//g;
-    $response = "Tärningen bestämmer: ".$choices[$choice];
-  }
-
-  $server->command("MSG $target $response") if $response;
-}
-
-sub autoop {
-  my ($server, $msg, $nick, $address, $target) = @_;
-
-  my @opers = ('Ades', 'anton', 'Angan', 'hunky\\', 'Tomas-');
-  my %hashop = map { $_ => 1 } @opers;
-
-  if ($msg eq "op plz" && exists($hashop{$nick})) {
-    $server->command("OP $target $nick");
-  } elsif ($msg eq "v plz") {
-    $server->command("VOICE $target $nick");
-  } else {
-    $server->command("MSG $target Nope.");
   }
 }
 
 sub get_page_title {
-  my ($url) = @_;
+  my ($url, $domain) = @_;
 
   my $ua = LWP::UserAgent->new(env_proxy=>1, keep_alive=>1, timeout=>5);
   $ua->agent($IRSSI{name}.".pl/$VERSION ".$ua->agent());
   my $res = $ua->get($url);
 
-  if ($res->is_success && $res->title()) {
-    return $res->title();
-  }
+  if ($res->is_success && $res->title) {
+    my $title = $res->title;
 
+    my @words = split(' ', $title);
+    my $tpos = undef;
+    my $pos = 0;
+
+    foreach my $word (@words) {
+      if (distance(lc($word), lc($domain)) < 2) {
+        $tpos = $pos;
+      }
+      $pos += 1;
+    }
+
+    if ($tpos) {
+      $words[$tpos] =~ s/^[,\.]|[,\.]$//;
+
+      if ($words[$tpos-1] && $words[$tpos-1] =~ "[-\|]") {
+        $title = join(' ', @words[0..$tpos-2]);
+      } elsif ($words[$tpos+1] && $words[$tpos+1] =~ "[-\|]") {
+        $title = join(' ', @words[$tpos+2..-1]);
+      }
+    }
+  
+    return "[$words[$tpos]] $title";
+  }
+  
   return 0;
 }
 
-# The below code is taken from and copyrighted by
-# Simon Lundstöm (http://soy.se/code/)
+### The below code is based on code copyrighted by
+### Simon Lundstöm (http://soy.se/code/)
 sub spotify {
   my ($kind, $id) = @_;
 
@@ -121,8 +123,8 @@ sub spotify {
   $ua->agent($IRSSI{name}.".pl/$VERSION ".$ua->agent());
   my $res = $ua->get($url);
 
-  if ($res->is_success()) {
-    my ($xml, $info) = (XMLin($res->content()), undef);
+  if ($res->is_success) {
+    my ($xml, $info) = (XMLin($res->content), undef);
 
     if ($xml->{'artist'}->{'name'}) {
       $info .= $xml->{'artist'}->{'name'};
@@ -147,9 +149,10 @@ sub spotify {
 
     return "[Spotify] $info";
   }
-
+  
   return 0;
 }
 
-Irssi::signal_add('message public', 'autoop');
-Irssi::signal_add('message public', 'autobot');
+Irssi::signal_add("message public", "auto_op");
+Irssi::signal_add("message public", "dice");
+Irssi::signal_add("message public", "uri_handler");
